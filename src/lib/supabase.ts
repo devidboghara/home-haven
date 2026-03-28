cc54de5abbbbefe9e09566097babb7b958008180
devidboghara <devidboghara001@gmail.com>
2026-03-29 00:48:11 +0530

    database connection successfull

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// Check if variables are missing
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase URL or Anon Key in Environment Variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
